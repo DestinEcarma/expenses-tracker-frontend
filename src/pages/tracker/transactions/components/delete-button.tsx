a1c4b0ebfd61016d8f817d90c4b6eb6d5bf58409
MakerDoe <109215379+MakerDoe@users.noreply.github.com>
2024-05-22 11:18:45 +0800

    chore: Add delete functionality to DeleteButton component

--- src/pages/tracker/transactions/components/delete-button.tsx
+++ src/pages/tracker/transactions/components/delete-button.tsx
@@ -2,69 +2,92 @@ import { FormEvent, useContext, useState } from "react";
 import { FaTrash } from "react-icons/fa6";
 import { DeleteTransaction } from "utilities/api";
 import { CategoryIdContext, useTransactionsContext } from "../utilities/transactions-context";
+import { StatusCode } from "utilities/status-code";
+import { useNavigate } from "react-router-dom";
 
 interface DeleteButtonProps {
 	id: string;
 }
 
-function DeleteButton({ id: transactionId }: DeleteButtonProps) {
-	const [isModalOpen, setIsModalOpen] = useState(0);
-	const categoryId = useContext(CategoryIdContext);
+interface ModalProps extends DeleteButtonProps {
+	closeModal: () => void;
+	isModalOpen: number;
+}
+
+function Modal({ closeModal, id, isModalOpen }: ModalProps) {
+	const [disabled, setDisabled] = useState(false);
+
 	const setTransactions = useTransactionsContext();
+	const categoryId = useContext(CategoryIdContext);
+	const navigate = useNavigate();
+
+	const onSubmit = (event: FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		setDisabled(true);
+
+		DeleteTransaction(categoryId, id)
+			.then((statusCode) => {
+				switch (statusCode) {
+					case StatusCode.OK:
+						setTransactions((prev) => prev.filter((item) => item.id !== id));
+						return;
+					case StatusCode.UNAUTHORIZED:
+						return navigate("/login");
+					default:
+						throw new Error(`Recieved an unexpected status code :: ${statusCode}.`);
+				}
+			})
+			.catch(alert)
+			.finally(closeModal);
+	};
+
+	return (
+		<div
+			data-open={isModalOpen}
+			className="z-50 fixed top-0 left-0 flex justify-center items-center w-full h-dvh bg-gray-500/30 backdrop-blur-sm data-[open='-1']:animate-fade-out data-[open='1']:animate-fade-in"
+		>
+			<form
+				onSubmit={onSubmit}
+				data-open={isModalOpen}
+				className="absolute w-max data-[open='-1']:animate-fade-out data-[open='1']:animate-fade-in"
+			>
+				<div className="left-0 flex flex-col gap-4 w-max bg-white p-4 rounded-md drop-shadow-md">
+					<h1 className="text-center text-black text-xl font-bold">Are you sure</h1>
+					<p className="text-gray-400">This action cannot be undone.</p>
+					<div className="flex-grow flex justify-between font-bold text-white">
+						<button
+							onClick={closeModal}
+							disabled={disabled}
+							type="button"
+							className="w-1/4 py-2 bg-blue-500 hover:bg-blue-600 rounded-md transition-colors shadow-md disabled:bg-gray-400"
+						>
+							No
+						</button>
+						<button
+							disabled={disabled}
+							type="submit"
+							className="w-1/4 py-2 bg-red-500 hover:bg-red-600 rounded-md transition-colors shadow-md disabled:bg-gray-400"
+						>
+							Yes
+						</button>
+					</div>
+				</div>
+			</form>
+		</div>
+	);
+}
+
+function DeleteButton({ id }: DeleteButtonProps) {
+	const [isModalOpen, setIsModalOpen] = useState(0);
 
 	const closeModal = () => {
 		setTimeout(() => setIsModalOpen(0), 200);
 		setIsModalOpen(-1);
 	};
 
-	const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
-		try {
-			event.preventDefault();
-
-			if ((await DeleteTransaction(categoryId, transactionId)) === 200) {
-				setTransactions((prev) => prev.filter((item) => item.id !== transactionId));
-			}
-
-			closeModal();
-		} catch (error) {
-			console.error(error);
-		}
-	};
-
 	return (
 		<>
-			{isModalOpen !== 0 && (
-				<div
-					data-open={isModalOpen}
-					className="z-50 fixed top-0 left-0 flex justify-center items-center w-full h-dvh bg-gray-500/30 backdrop-blur-sm data-[open='-1']:animate-fade-out data-[open='1']:animate-fade-in"
-				>
-					<form
-						onSubmit={onSubmit}
-						data-open={isModalOpen}
-						className="absolute w-max data-[open='-1']:animate-fade-out data-[open='1']:animate-fade-in"
-					>
-						<div className="left-0 flex flex-col gap-4 w-max bg-white p-4 rounded-md drop-shadow-md">
-							<h1 className="text-center text-black text-xl font-bold">Are you sure</h1>
-							<p className="text-gray-400">This action cannot be undone.</p>
-							<div className="flex-grow flex justify-between font-bold text-white">
-								<button
-									onClick={closeModal}
-									type="button"
-									className="w-1/4 py-2 bg-blue-500 hover:bg-blue-600 rounded-md transition-colors shadow-md"
-								>
-									No
-								</button>
-								<button
-									type="submit"
-									className="w-1/4 py-2 bg-red-500 hover:bg-red-600 rounded-md transition-colors shadow-md"
-								>
-									Yes
-								</button>
-							</div>
-						</div>
-					</form>
-				</div>
-			)}
+			{isModalOpen !== 0 && <Modal closeModal={closeModal} id={id} isModalOpen={isModalOpen} />}
 			<button
 				onClick={() =>
 					setIsModalOpen((prev) => {
